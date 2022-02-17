480a69bae9237cd9fb5cbc295aba859ef5956595
Ananya Sharma <ananyas237@gmail.com>
2022-02-17 01:36:51 -0500

    added hero section component

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,93 @@
+const HeroSection = () => {
+  return (
+    <div className="relative overflow-hidden pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
+      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static">
+        <div className="sm:max-w-lg">
+          <h1 className="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-6xl">
+            Let's get ready for your next adventure
+          </h1>
+          <p className="mt-4 text-xl text-gray-500">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+        <div>
+          <div className="mt-10">
+            <div
+              aria-hidden="true"
+              className="pointer-events-none lg:absolute lg:inset-y-0 lg:max-w-7xl lg:mx-auto lg:w-full"
+            >
+              <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                <div className="flex items-center space-x-6 lg:space-x-8">
+                  <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
+                    <div className="w-44 h-64 rounded-lg overflow-hidden sm:opacity-0 lg:opacity-100">
+                      <img
+                        src="https://images.unsplash.com/photo-1555367832-f5efe20f1d5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+                        alt=""
+                        className="w-full h-full object-center object-cover"
+                      />
+                    </div>
+                    <div className="w-44 h-64 rounded-lg overflow-hidden">
+                      <img
+                        src="https://images.unsplash.com/photo-1486915309851-b0cc1f8a0084?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+                        alt=""
+                        className="w-full h-full object-center object-cover"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
+                    <div className="w-44 h-64 rounded-lg overflow-hidden">
+                      <img
+                        src="https://images.unsplash.com/photo-1628087235616-4e146afcd061?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80"
+                        alt=""
+                        className="w-full h-full object-center object-cover"
+                      />
+                    </div>
+                    <div className="w-44 h-64 rounded-lg overflow-hidden">
+                      <img
+                        src="https://images.unsplash.com/photo-1570739260082-39a84dae80c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80"
+                        alt=""
+                        className="w-full h-full object-center object-cover"
+                      />
+                    </div>
+                    <div className="w-44 h-64 rounded-lg overflow-hidden">
+                      <img
+                        src="https://images.unsplash.com/photo-1623083617139-e455e92030e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+                        alt=""
+                        className="w-full h-full object-center object-cover"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
+                    <div className="w-44 h-64 rounded-lg overflow-hidden">
+                      <img
+                        src="https://images.unsplash.com/photo-1626194383653-d0d908e585fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+                        alt=""
+                        className="w-full h-full object-center object-cover"
+                      />
+                    </div>
+                    <div className="w-44 h-64 rounded-lg overflow-hidden">
+                      <img
+                        src="https://images.unsplash.com/photo-1622260614153-03223fb72052?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+                        alt=""
+                        className="w-full h-full object-center object-cover"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <a
+              href="#"
+              className="inline-block text-center bg-indigo-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-indigo-700"
+            >
+              Shop Collection
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
