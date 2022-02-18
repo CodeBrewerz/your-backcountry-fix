45d58522dfc1b9d8ef32b2e6a2e0a67515987b03
Sumit Mittal <sumit22mittal@gmail.com>
2022-02-18 10:37:32 -0500

    added product review and related purchase sections

---
+++ app/components/ProductReviewsSection.jsx
@@ -0,0 +1,68 @@
+import { StarIcon } from "@heroicons/react/solid";
+
+const ProductReviewsSection = ({ reviews }) => {
+  return (
+    <section aria-labelledby="reviews-heading" className="mt-16 sm:mt-24">
+      <h2 id="reviews-heading" className="text-lg font-medium text-gray-900">
+        Recent reviews
+      </h2>
+
+      <div className="mt-6 border-t border-b border-gray-200 pb-10 divide-y divide-gray-200 space-y-10">
+        {reviews.length > 0 ? (
+          reviews.map((review) => (
+            <div
+              key={review.id}
+              className="pt-10 lg:grid lg:grid-cols-12 lg:gap-x-8"
+            >
+              <div className="lg:col-start-5 lg:col-span-8 xl:col-start-4 xl:col-span-9 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:items-start">
+                <div className="flex items-center xl:col-span-1">
+                  <div className="flex items-center">
+                    {[0, 1, 2, 3, 4].map((rating) => (
+                      <StarIcon
+                        key={rating}
+                        className={classNames(
+                          review.rating > rating
+                            ? "text-yellow-400"
+                            : "text-gray-200",
+                          "h-5 w-5 flex-shrink-0"
+                        )}
+                        aria-hidden="true"
+                      />
+                    ))}
+                  </div>
+                  <p className="ml-3 text-sm text-gray-700">
+                    {review.rating}
+                    <span className="sr-only"> out of 5 stars</span>
+                  </p>
+                </div>
+
+                <div className="mt-4 lg:mt-6 xl:mt-0 xl:col-span-2">
+                  <h3 className="text-sm font-medium text-gray-900">
+                    {review.title}
+                  </h3>
+
+                  <div className="mt-3 space-y-6 text-sm text-gray-500">
+                    {review.content}
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-6 flex items-center text-sm lg:mt-0 lg:col-start-1 lg:col-span-4 lg:row-start-1 lg:flex-col lg:items-start xl:col-span-3">
+                <p className="font-medium text-gray-900">{review.author}</p>
+                <p className="ml-4 border-l border-gray-200 pl-4 text-gray-500 lg:ml-0 lg:mt-2 lg:border-0 lg:pl-0">
+                  {review.date}
+                </p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="pt-5 text-lg font-medium">
+            No reviews yet. Be the first one to review this product.
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default ProductReviewsSection;
---
+++ app/components/RelatedPurchaseSection.jsx
@@ -0,0 +1,40 @@
+const RelatedPurchaseSection = ({ relatedProducts }) => {
+  return (
+    <section
+      aria-labelledby="related-heading"
+      className="mt-10 py-16 px-4 sm:px-0"
+    >
+      <h2 id="related-heading" className="text-xl font-bold text-gray-900">
+        Customers also purchased
+      </h2>
+      <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        {relatedProducts.map((product) => (
+          <div key={product.id} className="group relative">
+            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+              <img
+                src={product.imageSrc}
+                alt={product.imageAlt}
+                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+              />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href={product.href}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {product.name}
+                  </a>
+                </h3>
+              </div>
+              <p className="text-sm font-medium text-gray-900">
+                {product.price}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default RelatedPurchaseSection;
